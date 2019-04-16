aeffa287edb8c69ee9af172427baf53666a5a915
Jaime Longoria <jaime.longoria@journalism.cuny.edu>
2019-04-16 16:17:07 -0400

    radio buttons working, need to make the rest

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	$("input[type='radio']").click(function () {
+		var radioValue = $("input[name='monday']:checked").val();
+		if (radioValue);
+
+		console.log(radioValue);
+		console.log("radioValue");
+
+
+
+
+	});
+
+});
