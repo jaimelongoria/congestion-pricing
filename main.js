617a83f9a1fc80b4dad77b25b297a29f9cc74be7
Jaime Longoria <jaime.longoria@journalism.cuny.edu>
2019-04-17 01:05:36 -0400

    added css and jq

--- main.js
+++ main.js
@@ -1,14 +1,44 @@
 $(document).ready(function () {
-	$("input[type='radio']").click(function () {
-		var radioMonday = $("input[name='monday']:checked").val();
-		if (radioMonday);
+	$('#btn').click(function () {
+		var mon = $('input[name=monday]:checked').val();
+		var tues = $('input[name=tuesday]:checked').val();
+		var wed = $('input[name=wednesday]:checked').val();
+		var thur = $('input[name=thursday]:checked').val();
+		var fri = $('input[name=friday]:checked').val();
+		var sat = $('input[name=saturday]:checked').val();
+		var sun = $('input[name=sunday]:checked').val();
 
-		console.log(radioMonday);
-		console.log("radioMonday");
+		console.log(mon);
+		console.log(tues);
+		console.log(wed);
+		console.log(thur);
+		console.log(fri);
+		console.log(sat);
+		console.log(sun);
 
+		let finalmon = parseFloat(mon);
+		let finaltues = parseFloat(tues);
+		let finalwed = parseFloat(wed);
+		let finalthurs = parseFloat(thur);
+		let finalfri = parseFloat(fri);
+		let finalsat = parseFloat(sat);
+		let finalsun = parseFloat(sun);
 
+		console.log(finalsun)
 
+		let finalvalue = finalmon + finaltues + finalwed + finalthurs + finalfri + finalsat + finalsun;
+		let avalue = finalvalue * 52
+		let mvalue = avalue / 12
+		console.log(finalvalue);
+		console.log("finalvalue");
+		console.log(avalue);
+		console.log("avalue");
+		console.log(mvalue);
+		console.log("mvalue");
+
+		$("#tcost").find("span").text(finalvalue);
+		$("#mcost").find("span").text(mvalue);
+		$("#acost").find("span").text(avalue);
 
 	});
-
 });
